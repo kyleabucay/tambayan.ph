8227bbd0274adc4a2936993d5a665fc83eac9b89
Kyle Abucay <kyleangeloabucay@gmail.com>
2025-07-24 23:42:18 +0800

    added dorm details and its styles

--- src/components/pages/DormDetails.jsx
+++ src/components/pages/DormDetails.jsx
@@ -1,5 +1,13 @@
+import { useLocation, useParams } from "react-router-dom"
+import "../styles/dorm-details.css"
+
 export default function DormDetails() {
+    const { id } = useParams()
+    console.log(id)
+
     return (
-        <h1>Dorm details go here</h1>
+        <div className="dorm-details">
+            <h1>Dorm details go here</h1>
+        </div>
     )
 }
