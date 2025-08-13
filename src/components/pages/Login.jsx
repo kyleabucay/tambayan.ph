e0752294a7c0796c9e984c15215976150e878f18
Kyle Abucay <kyleangeloabucay@gmail.com>
2025-08-13 14:23:58 +0800

    case update for login page

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import Card from "../ui/card";
+import Card from "../ui/Card";
 import CardHeader from "../ui/CardHeader";
 import CardContent from "../ui/CardContent";
 import Button from "../ui/Button"
